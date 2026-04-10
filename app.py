dfef260edc50781a9504a05574e2cac4c2763f3c
Wayna Ali <waynaali9@gmail.com>
2026-04-10 13:16:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_gauge(prob):
         title={'text': "Churn Risk (%)"},
         gauge={
             'axis': {'range': [0, 100]},
-            'bar': {'color': "#333333"},
+            'bar': {'color': "yellow"},
             'steps': [
                 {'range': [0, 40], 'color': "green"},
                 {'range': [40, 70], 'color': "orange"},
